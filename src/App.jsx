efd3fb4e303183d44ffd449d8a69c944c0830c13
Adam <schrock-adam@hotmail.com>
2023-07-07 11:38:11 -0400

    starting on login component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
+import Header from './Components/Header';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './Components/Login';
 
 function App() {
-  return <div>hola</div>;
+  return (
+    <div className='App'>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Login />} />
+          {/* <Route path='/home' element={<Home />} />
+          <Route path='/detail/:id' element={<Detail />} /> */}
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
